e528566e8091190bb845b3156e4839b18a7e526f
Tilman Holube <tilman@holube.de>
2024-06-04 13:34:14 +0200

    kurtama update with icon

--- packages/kurtama-client/default.nix
+++ packages/kurtama-client/default.nix
@@ -3,6 +3,7 @@ let
 in
 { stdenv ? pkgs.stdenv
 , fetchurl ? pkgs.fetchurl
+, fetchFromGitHub ? pkgs.fetchFromGitHub
 , makeWrapper ? pkgs.makeWrapper
 , jdk21 ? pkgs.jdk21
 , makeDesktopItem ? pkgs.makeDesktopItem
@@ -10,12 +11,19 @@ in
 
 stdenv.mkDerivation rec {
   pname = "kurtama-client";
-  kurtama-version = "0.0.1-alpha+20240318135836";
+  kurtama-version = "0.0.1-alpha+20240603112143";
   version = "${kurtama-version}";
 
-  src = fetchurl {
+  jar = fetchurl {
     url = "https://github.com/schlunzis/Kurtama/releases/download/v${kurtama-version}/kurtama-client-${kurtama-version}-linux.jar";
-    hash = "sha256-5+IuL8bAemKfa8aZiqXoKuduCQdrkUGGsQNEhJUtpdI=";
+    hash = "sha256-YSrttCIAHWts6faNRLZoCdjypwf9SBme6TCzK2fpkUo=";
+  };
+
+  src = fetchFromGitHub {
+    owner = "schlunzis";
+    repo = "Kurtama";
+    rev = "v${version}";
+    hash = "sha256-sJl+zzXGKWzmuTqwYfVoSIeiyv/2PVeAZ/JqUp1G5M4=";
   };
 
   dontUnpack = true;
@@ -23,15 +31,17 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
-    mkdir -pv $out/share/java $out/bin
-    cp ${src} $out/share/java/${pname}.jar
+        mkdir -pv $out/share/java $out/bin
+        cp ${jar} $out/share/java/${pname}.jar
 
-    makeWrapper ${jdk21}/bin/java $out/bin/kurtama-client \
-      --add-flags "-jar $out/share/java/${pname}.jar" \
-      --set _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on' \
-      --set _JAVA_AWT_WM_NONREPARENTING 1
+        makeWrapper ${jdk21}/bin/java $out/bin/kurtama-client \
+        --add-flags "-jar $out/share/java/${pname}.jar" \
+        --set _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on' \
+        --set _JAVA_AWT_WM_NONREPARENTING 1
 
-    ln -sv "$desktopItem/share/applications" $out/share/
+        ln -sv "$desktopItem/share/applications" $out/share/
+        mkdir -v $out/share/pixmaps
+    		cp -v $src/client/src/main/resources/icons/icon-512.png $out/share/pixmaps/kurtama-client.png
   '';
 
   desktopItem = makeDesktopItem {
